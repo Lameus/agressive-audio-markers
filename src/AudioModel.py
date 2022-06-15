be689099ccd7d6545a9cd1123390f93457f9084a
Ivan Smirnov <34346831+Lameus919@users.noreply.github.com>
2022-06-15 13:05:16 +0300

    Add files via upload

---
+++ src/AudioModel.py
@@ -0,0 +1,74 @@
+import torch
+from torch import nn
+import numpy as np
+
+
+class AudioModel(nn.Module):
+    def __init__(self):
+        super(AudioModel, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=1,
+                out_channels=512,
+                kernel_size=3,
+                stride=1,
+                padding=2
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+            nn.BatchNorm2d(512)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=512,
+                out_channels=256,
+                kernel_size=3,
+                stride=1,
+                padding=2
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+            nn.BatchNorm2d(256)
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=256,
+                out_channels=128,
+                kernel_size=3,
+                stride=1,
+                padding=2
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+            nn.BatchNorm2d(128)
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=128,
+                out_channels=64,
+                kernel_size=3,
+                stride=1,
+                padding=2
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+            nn.BatchNorm2d(64)
+        )
+        self.flatten = nn.Flatten()
+        self.dropout = nn.Dropout(p=0.3)
+        self.linear = nn.Linear(5504, 7)
+        self.softmax = nn.Softmax(dim=1)
+
+
+    def forward(self, input_data):
+        x = self.conv1(input_data)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.flatten(x)
+        x = self.dropout(self.flatten(x))
+        logits = self.linear(x)
+        predictions = self.softmax(logits)
+
+        return predictions
+
---
+++ src/Torch_emotion.py
@@ -0,0 +1,73 @@
+import torch
+import torchaudio
+import numpy as np
+from src.utils import chunkizer
+
+from src.AudioModel import AudioModel
+
+
+class Torch_emotion:
+    # Заменить audio_path на False и по умолчанию передавать chunks
+    def __init__(self, model, signal, device='cpu', sr=44100):
+        self.signal = signal
+        self.device = device
+
+        self.model = model
+        self.sr = sr
+        self.num_samples = 3 * sr
+        self.target_sample_rate = sr
+        self.to_voice = torchaudio.transforms.Vad(self.sr)
+
+        self.predict = []  # Значения эмоций
+
+    def audio_pipeline(self):
+        # self.signal = self.to_voice(self.signal)
+        self.chunks = chunkizer(3, self.signal[0].numpy(), self.sr)
+        self.chunks = [torch.Tensor(i) for i in self.chunks]  # Перевод фрагментов в тензоры
+        self.test = []  # Лист для записи преобразованных фрагментов
+        for i in range(len(self.chunks)):
+            # Преобразование к одному значению sample rate
+            w = self.chunks[i]
+            w.unsqueeze_(0)
+            w = self._cut_if_necessary(w)
+            w = self._right_pad_if_necessary(w)
+
+            mfcc = torchaudio.transforms.MFCC(sample_rate=self.sr,
+                                              n_mfcc=13)(w)
+
+            mfcc = np.transpose(mfcc.numpy(), (1, 2, 0))
+            mfcc = np.transpose(mfcc, (2, 0, 1)).astype(np.float32)
+
+            self.test.append(torch.tensor(mfcc, dtype=torch.float))
+        # Перевод списка тензоров к тензору тензоров
+        # self.test = torch.stack(self.test).to(self.device)
+
+        # Предсказание
+        self.model.eval()
+        for i in range(len(self.test)):
+            c = self.test[i]
+            c.unsqueeze_(0)
+            # Добавление значения в список предсказаний
+            self.predict.append(self.model(c).to('cpu').detach().numpy()[0])
+            
+
+        return self.predict
+
+    # Функции обработки аудио
+    def _cut_if_necessary(self, signal):
+        if signal.shape[1] > self.num_samples:
+            signal = signal[:, :self.num_samples]
+        return signal
+
+    def _right_pad_if_necessary(self, signal):
+        length_signal = signal.shape[1]
+        if length_signal < self.num_samples:
+            num_missing_samples = self.num_samples - length_signal
+            last_dim_padding = (0, num_missing_samples)
+            signal = torch.nn.functional.pad(signal, last_dim_padding)
+        return signal
+
+
+# if __name__ == '__main__':
+#     Test = Torch_emotion(audio_path='C:/Users/1vany/Desktop/Disktop/Python/АудиоТЗ/фрагменты/0.0_0.0.mp3')
+#     print(Test.audio_pipeline())
