ebdff053050c8f46bd2b4cc6dbb9c9669617339f
Ivan Smirnov <34346831+Lameus@users.noreply.github.com>
2022-09-01 15:49:04 +0300

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,18 +1,7 @@
-import math
-import warnings
-
-import librosa
-
-warnings.filterwarnings('ignore')
-
-
-
-# Делит аудио файл(фрагменты) на чанки
 def chunkizer(chunk_length, audio, sr):
-    num_chunks = math.ceil(librosa.get_duration(audio, sr=sr) / chunk_length)
+    duration = audio.shape[0] / sr
+    num_chunks = int(-(-duration // chunk_length)) # ceil without import math
     chunks = []
     for i in range(num_chunks):
         chunks.append(audio[i * chunk_length * sr:(i + 1) * chunk_length * sr])
     return chunks
-
-
